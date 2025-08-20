10c7a4eb1ff14f0ea8a108cd24592c4a2b28f529
isuuraa <mradhityaaldiansyah@gmail.com>
2025-08-20 09:59:50 +0700

    Update.php

--- landing/landing_page.php
+++ landing/landing_page.php
@@ -11,7 +11,7 @@
     <!-- Header -->
     <header class="bg-[#00646A] p-4 flex flex-col gap-2">
         <div class="flex items-center justify-between">
-            <img src="../foto/logo.png" alt="Logo" class="px-6 py-2 rounded w-64 h-30 object-contain">
+            <img src="../img/logo.png" alt="Logo" class="px-6 py-2 rounded w-64 h-30 object-contain">
         </div>
         <div class="flex justify-end gap-4 mt-2">
             <button class="bg-white px-4 py-1 rounded text-xs">Info & Tips Kerja</button>
@@ -147,7 +147,7 @@
     <!-- Footer -->
     <footer class="bg-gray-200 mt-auto py-6 px-4">
         <div class="max-w-4xl mx-auto flex flex-col md:flex-row justify-between items-center gap-4">
-            <img src="../foto/logo.png" class="bg-[#00646A] px-6 py-2 rounded w-64 h-20 object-contain">
+            <img src="../img/logo.png" class="bg-[#00646A] px-6 py-2 rounded w-64 h-20 object-contain">
             <div class="flex gap-4">
                 <div class="flex flex-col gap-1">
                 <div>
