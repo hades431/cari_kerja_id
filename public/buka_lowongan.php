dcbdf4f1d86b8c5a055c0a06b279a0c17bd81e2b
haikalkasep <haikaldks@gmail.com>
2025-08-20 10:15:01 +0700

    logo

---
+++ public/buka_lowongan.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buka Lowongan</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-gray-100 flex flex-col min-h-screen">
+        <header class="bg-[#00646A] p-4 flex flex-col gap-2">
+        <div class="flex items-center justify-between">
+            <img src="../img/logo2.png" alt="Logo" class="px-6 py-2 rounded w-64 h-30 object-contain">
+        </div>
+        <div class="flex justify-end gap-4 mt-2">
+            <button class="bg-white px-4 py-1 rounded text-xs">Info & Tips Kerja</button>
+            <button class="bg-white px-4 py-1 rounded text-xs">Login</button>
+        </div>
+    </header>
+</body>
+</html>
