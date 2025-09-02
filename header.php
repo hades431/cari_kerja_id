bbd1b7eeef5300679ff355c819e44ee0841acc03
haikalkasep <haikaldks@gmail.com>
2025-09-02 10:29:48 +0700

    update logic header

--- header.php
+++ header.php
@@ -16,11 +16,13 @@
             <img src="../img/logo2.png" alt="Logo" class="px-2 py-0 rounded w-64 h-30 object-contain">
             <div class="flex flex-col items-end gap-1">
                 <!-- Profil -->
-                <a href="../public/profil_pelamar.php" class="flex items-center gap-2 mb-0 hover:opacity-80 transition">
-                    <img src="../img/avatar.png" alt="Profil"
-                        class="w-10 h-10 rounded-full border-2 border-white shadow">
-                    <span class="text-white font-semibold">Nama Pengguna</span>
-                </a>
+                <?php if (!isset($hide_header_profile) || !$hide_header_profile): ?>
+                    <a href="../public/profil_pelamar.php" class="flex items-center gap-2 mb-0 hover:opacity-80 transition">
+                        <img src="../img/avatar.png" alt="Profil"
+                            class="w-10 h-10 rounded-full border-2 border-white shadow">
+                        <span class="text-white font-semibold">Nama Pengguna</span>
+                    </a>
+                <?php endif; ?>
                 <div class="flex gap-3 mt-2">
                     <button class="bg-green-500 max-w-max hover:bg-green-600 active:bg-green-700 
                       text-white px-6 py-2 rounded-full font-bold shadow transition">Info & Tips Kerja</button>
