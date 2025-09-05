2913d65704a3718d10a1c737bb7befd9c9a3ae6b
yuwphy <wafakusmawati112@gmail.com>
2025-09-05 16:35:26 +0700

    update atmin hapus artikel

--- admin/hapus_artikel.php
+++ admin/hapus_artikel.php
@@ -1,20 +1,15 @@
 <?php
 session_start();
-include '../function/logic.php'; // sudah ada koneksi $conn di sini kan?
-
+include '../function/logic.php'; 
 if (isset($_GET['id'])) {
-    $id = intval($_GET['id']); // amankan input
-
-    // Ambil data artikel untuk cek apakah ada gambar
+    $id = intval($_GET['id']); 
     $result = mysqli_query($conn, "SELECT gambar FROM artikel WHERE id='$id'");
     $artikel = mysqli_fetch_assoc($result);
 
     if ($artikel) {
-        // Hapus record dari database
         $delete = mysqli_query($conn, "DELETE FROM artikel WHERE id='$id'");
 
         if ($delete) {
-            // Jika ada gambar, hapus file-nya juga
             if (!empty($artikel['gambar'])) {
                 $filePath = "../img/" . $artikel['gambar'];
                 if (file_exists($filePath)) {
@@ -30,8 +25,6 @@ if (isset($_GET['id'])) {
         $_SESSION['error'] = "Artikel tidak ditemukan!";
     }
 }
-
-// Balik lagi ke halaman daftar artikel
 header("Location: tips_kerja_artikel.php");
 exit;
 ?>
