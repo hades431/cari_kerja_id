ab2955211c6dbb5c698c1c1d2ba2eaa48ea2c640
isuuraa <mradhityaaldiansyah@gmail.com>
2025-09-08 10:28:25 +0700

    Update lagi

---
+++ about/about_us.php
@@ -0,0 +1,28 @@
+<?php
+
+$judul_halaman = 'About Us';
+include '../header.php';
+
+?>
+
+<section class="max-w-3xl mx-auto bg-white rounded-2xl shadow p-8 mt-8 mb-12">
+    <h1 class="text-4xl md:text-5xl font-semibold text-center text-[#00646A] mb-6 leading-tight">
+        <span class="font-bold">About</span> Us
+    </h1>
+    <p class="text-lg text-gray-700 mb-4">
+       Cari Kerja ID adalah platform pencarian kerja yang membantu kamu menemukan lowongan kerja dengan cepat, mudah, dan terpercaya. Kami menghubungkan para pencari kerja dengan perusahaan di seluruh Indonesia, dari berbagai bidang dan level karier.
+    </p>
+    <p class="text-lg text-gray-700 mb-4">
+        Misi kami yaitu mempermudah perjalanan kariermu dan mendukung perusahaan menemukan talenta terbaik. Dengan sistem pencarian yang praktis, informasi yang selalu update, dan fitur yang aman, kami percaya pekerjaan impianmu ada di sini.
+    </p>
+    <p class="text-lg text-gray-700 mb-4">
+        Cari Kerja ID is a job search platform that helps you find the right job quickly, easily, and reliably. We connect job seekers with companies across Indonesia, covering various industries and career levels.
+    </p>
+    <p class="text-lg text-gray-700">
+       Our mission is to make your career journey easier and help companies discover top talent. With practical search tools, up-to-date information, and secure features, we believe your dream job is here.
+    </p>
+</section>
+
+<?php
+include '../footer.php';
+?>
