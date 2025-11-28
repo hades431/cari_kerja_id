2a1b3e5605371aa65b8b7359719e82354a49caf9
mdias26 <yazz261107@gmail.com>
2025-11-28 19:58:34 +0700

    minimal 8 karakter

--- function/auth.php
+++ function/auth.php
@@ -3,27 +3,33 @@ include '../function/logic.php';
 
 function register($data){
     global $conn;
-$username = $_POST['username'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$result = mysqli_query($conn,"SELECT*FROM user WHERE email = '$email' ");
-$konfirm = $_POST['konfirmasi_password'];
-if($password != $konfirm){
-            echo "<script>alert('Password dan Confirm Password tidak sama!');</script>";
-    return false;
-}
-if(mysqli_num_rows($result) > 0){
-    echo "<script>alert('Email sudah terdaftar!');</script>";
-    return false;
-}
-if($password <= 7){
-    header("location: ../login/register.php?error=password");
-    return false;
-}
-$passwordhash = password_hash($password,PASSWORD_DEFAULT);
-$query_insert = "INSERT INTO user (username,email,password,role,status_akun) VALUES('$username','$email','$passwordhash','pelamar','aktif')";
-mysqli_query($conn,$query_insert);
-return mysqli_affected_rows($conn);
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $result = mysqli_query($conn,"SELECT*FROM user WHERE email = '$email' ");
+    $konfirm = $_POST['konfirmasi_password'];
+
+    if($password != $konfirm){
+        echo "<script>alert('Password dan Confirm Password tidak sama!');</script>";
+        return false;
+    }
+
+    // tambahkan validasi: minimal 8 karakter dan hanya huruf atau angka
+    if (!preg_match('/^[A-Za-z0-9]{8,}$/', $password)) {
+        // kembalikan ke halaman register dengan flag error
+        header("location: ../login/register.php?error=password");
+        return false;
+    }
+
+    if(mysqli_num_rows($result) > 0){
+        echo "<script>alert('Email sudah terdaftar!');</script>";
+        return false;
+    }
+
+    $passwordhash = password_hash($password,PASSWORD_DEFAULT);
+    $query_insert = "INSERT INTO user (username,email,password,role,status_akun) VALUES('$username','$email','$passwordhash','pelamar','aktif')";
+    mysqli_query($conn,$query_insert);
+    return mysqli_affected_rows($conn);
 }
 
 ?>
